1fefbcb03f28ee6b0618154734e54f2062cfd0d5
alex <alex.ochs2108@gmail.com>
2017-10-28 22:57:30 +0200

    drug / weed fixed

--- FromNothingToBoss/src/com/greenStudios/objects/Drug.java
+++ FromNothingToBoss/src/com/greenStudios/objects/Drug.java
@@ -1,6 +1,12 @@
 package com.greenStudios.objects;
 
-public class Drug {
+public class Drug extends WorldObject{
+	
+	protected int mSellValue;
+	protected int mBuyValue;
+	protected int mPlanttime;
+	protected int mHarvesttime;
+	protected int mGrowtime;
 	
 	
 
