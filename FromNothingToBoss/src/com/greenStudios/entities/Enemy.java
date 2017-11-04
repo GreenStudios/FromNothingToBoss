4502d2d7c45e91651cbc8419ebf40763bdbe24e5
yori <alex.ochs2108@gmail.com>
2017-11-04 17:29:08 +0100

    added NPC/Enemy

---
+++ FromNothingToBoss/src/com/greenStudios/entities/Enemy.java
@@ -0,0 +1,40 @@
+package com.greenStudios.entities;
+
+import java.awt.Graphics;
+
+import com.greenStudios.main.Handler;
+
+public class Enemy extends Creature{
+
+	public Enemy(Handler handler, float x, float y) {
+		super(handler, x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HEIGHT);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void die() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Type getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
